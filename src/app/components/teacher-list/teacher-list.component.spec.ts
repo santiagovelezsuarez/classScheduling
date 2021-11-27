64ab6ea73d096da5f790ff84f4fac800710752a6
JuanZapataV2 <juanp.zapataa@autonoma.edu.co>
2021-11-26 23:14:21 -0500

    Teacher list component's tests done

--- src/app/components/teacher-list/teacher-list.component.spec.ts
+++ src/app/components/teacher-list/teacher-list.component.spec.ts
@@ -1,25 +1,64 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { TeacherService } from 'src/app/services/teacher.service';
 
 import { TeacherListComponent } from './teacher-list.component';
 
-xdescribe('TeacherListComponent', () => {
+describe('TeacherListComponent', () => {
   let component: TeacherListComponent;
   let fixture: ComponentFixture<TeacherListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TeacherListComponent ]
-    })
-    .compileComponents();
-  });
+  const teachers = [
+    {
+      id: 1,
+      name: "Julia",
+      department_id: 1
+    },
+    {
+      id: 2,
+      name: "Jaime",
+      department_id: 1
+    }
+  ];
 
   beforeEach(() => {
+    let teacherServiceStub : Partial<TeacherService>;
+    teacherServiceStub = {
+      getTeachers: () => of(teachers)
+    };
+    TestBed.configureTestingModule({
+      declarations: [ TeacherListComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [{provide: TeacherService, useValue: teacherServiceStub}]
+    });
+
     fixture = TestBed.createComponent(TeacherListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('get teachers', () => {    
+    component.ngOnInit();
+    expect(component.teachers).toEqual(teachers);
+  });
+
+
+  it('search a teacher', () => {    
+    component.ngOnInit();
+    let res = component.filter;
+    expect(res.length).toEqual(2);
+    component.text = "jul";
+    component.search();
+    res = component.filter;
+    expect(res.length).toEqual(1);
+    component.text = "jai";
+    component.search();
+    res = component.filter;
+    expect(res.length).toEqual(1);
   });
 });
